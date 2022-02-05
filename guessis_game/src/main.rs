3150f0f04751913a37e93047bac6a61e70e75494
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2022-02-05 23:22:13 +0200

    add rand number

--- guessis_game/src/main.rs
+++ guessis_game/src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use rand::{thread_rng, Rng};
 
 
 
@@ -6,11 +7,19 @@ use std::io;
 
 fn main() {
     println!("Guess the number");
+
+    let mut rng =  rand::thread_rng();
+    let  secret_number = rng.gen_range(1..25);
+
+    
+
     println!("Please enter a number");
 
     let mut guess = String::new();
 
     io::stdin().read_line(&mut guess).expect("Can not read your number");
 
-    print!("You enter : {} ", guess);
+    println!("Secret number is {} ", secret_number);
+
+    println!("You enter : {} ", guess);
 }
