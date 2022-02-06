a2c40efd88f0cb2ee3aa2c457c7c1041dca72caa
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2022-02-06 10:46:54 +0200

    add loop

--- guessis_game/src/main.rs
+++ guessis_game/src/main.rs
@@ -7,28 +7,34 @@ use std::cmp::Ordering;
 
 
 fn main() {
-    println!("Guess the number");
-
-    let mut rng =  rand::thread_rng();
-    let  secret_number = rng.gen_range(1..25);
-
     
 
-    println!("Please enter a number");
+    
+    
+    
+
+    loop {
+        let mut rng =  rand::thread_rng();
+    let  secret_number = rng.gen_range(1..25);
 
     let mut guess = String::new();
+        println!("Guess the number");
+        println!("Please enter a number");
+        io::stdin().read_line(&mut guess).expect("Can not read your number");
+        let guess: u32 = guess.trim().parse().expect("Please enter a number");
 
-    io::stdin().read_line(&mut guess).expect("Can not read your number");
-    let guess: u32 = guess.trim().parse().expect("Please enter a number");
+        println!("Secret number is {} ", secret_number);
 
-    println!("Secret number is {} ", secret_number);
+        println!("You enter : {} ", guess);
 
-    println!("You enter : {} ", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("The number is less"),
-        Ordering::Greater => println!("The number is bigger"),
-        Ordering::Equal => println!("You are right"),   
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("The number is less"),
+            Ordering::Greater => println!("The number is bigger"),
+            Ordering::Equal => println!("You are right"),   
+        }
     }
+
+
+    
 
 }
